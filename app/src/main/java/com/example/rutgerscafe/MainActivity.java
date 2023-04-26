f5db339eec67aa94cc5ab30427e000cbc34deecb
Your Name <you@example.com>
2023-04-26 13:35:35 -0400

    .

--- app/src/main/java/com/example/rutgerscafe/MainActivity.java
+++ app/src/main/java/com/example/rutgerscafe/MainActivity.java
@@ -4,6 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
+import android.widget.*;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
