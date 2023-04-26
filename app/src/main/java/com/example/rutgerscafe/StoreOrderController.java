df51d471baa0482b35149485fd5ba249a186c053
Your Name <you@example.com>
2023-04-26 15:49:31 -0400

    .

---
+++ app/src/main/java/com/example/rutgerscafe/StoreOrderController.java
@@ -0,0 +1,14 @@
+package com.example.rutgerscafe;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class StoreOrderController extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.store_view);
+    }
+}
