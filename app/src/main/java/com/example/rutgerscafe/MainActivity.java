1496539e6f8bd728582c122f792734b9ad49dab7
Your Name <you@example.com>
2023-04-26 13:46:37 -0400

    .

--- app/src/main/java/com/example/rutgerscafe/MainActivity.java
+++ app/src/main/java/com/example/rutgerscafe/MainActivity.java
@@ -10,13 +10,14 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity{
-    Button coffeeButton, checkCartButton;
+    Button coffeeButton, checkCartButton, donutButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         coffeeButton = (Button) findViewById(R.id.coffeeButton);
         checkCartButton = (Button) findViewById(R.id.checkCartButton);
+        donutButton = (Button) findViewById(R.id.donutButton);
 
         coffeeButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -25,11 +26,18 @@ public class MainActivity extends AppCompatActivity{
                 startActivity(i);
             }
         });
-
         checkCartButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent i = new Intent(getApplicationContext(), basketController.class);
+                startActivity(i);
+            }
+        });
+
+        donutButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View V) {
+                Intent i = new Intent(getApplicationContext(), DonutController.class);
                 startActivity(i);
             }
         });
